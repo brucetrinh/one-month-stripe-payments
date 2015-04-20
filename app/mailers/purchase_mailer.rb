d57b91165608adcc0e595d324e76d4d86834d2c0
Bruce Trinh <bruce.trinh@yahoo.com.au>
2015-04-20 15:40:38 +1000

    Reworked views to use product information from the database

--- app/mailers/purchase_mailer.rb
+++ app/mailers/purchase_mailer.rb
@@ -3,7 +3,8 @@ class PurchaseMailer < ActionMailer::Base
 	default from: "One Month Stripe Payments <payments@onemonth.com>"
 
 	def purchase_receipt purchase
-		@purchase = purchase
+		@purchase = Purchase
+		@product = Product.find(purchase.product_id)
 		mail to: purchase.email, subject: "Thanks for your purchase"
 	end
 
